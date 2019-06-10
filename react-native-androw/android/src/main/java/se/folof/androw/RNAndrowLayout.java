f7cf02ed9c577eb61a7f45c24cc380fff23e21c4
Olof Larsson <olof@folof.se>
2019-06-10 09:44:04 +0200

    Removed unused imports

--- react-native-androw/android/src/main/java/se/folof/androw/RNAndrowLayout.java
+++ react-native-androw/android/src/main/java/se/folof/androw/RNAndrowLayout.java
@@ -8,8 +8,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewParent;
 
